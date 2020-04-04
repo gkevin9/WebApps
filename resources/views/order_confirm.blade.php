b9863e5cbccf4410fbeb0eae4b47ae529c61596c
adriank24 <41040039+adriank24@users.noreply.github.com>
2020-04-04 17:02:21 +0700

    Orders
    Page-page untuk order

---
+++ resources/views/order_confirm.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.master')
+@section('class')
+<li><a href="/">home</a></li>
+<li><a href="/menu">Menu</a></li>
+<li><a href="/about">About</a></li>
+<li><a class="active" href="/order">Order</a></li>
+@endsection
+
+@section('konten')
+    <!-- bradcam_area_start -->
+    <div class="bradcam_area breadcam_bg overlay">
+        <h3>Your Order is Made Right Now</h3>
+    </div>
+    <!-- bradcam_area_end -->
+
+@endsection
